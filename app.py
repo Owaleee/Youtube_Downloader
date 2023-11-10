01f9137457d69f6e4a806462134d77427673b420
Alexandre Queiroz <xandyzh65@gmail.com>
2023-11-10 19:21:34 -0300

    Função de Confirmação
    Função de confirmação adicionada, além de uma variável 'video' que faz o app ser mais estável

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from pytube import YouTube
 
-link = input("Qual o link do video que você quer baixar? ")
-YouTube = YouTube(link)
+while True:
+    link = input("Qual o link do vídeo que você quer baixar? ")
+    video = YouTube(link)
 
-video = YouTube.streams.get_highest_resolution()
-video.download()
-print('Done!!')
+    # Obtém o título do vídeo
+    titulo = video.title
+    print(f'\nTítulo do vídeo: {titulo}\n')
 
+    confirmacao = input(f'Esse é o titulo do video que você quer baixar? [S/N]\n')
+    if confirmacao == 's':
+        video = video.streams.get_highest_resolution()
+        video.download()
+        print(f'Pronto, vídeo baixado!')
+
+    elif confirmacao == 'n':
+        continue
+
+    else:
+        print('Opção inválida. Por favor, digite S ou N.')
