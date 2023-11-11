86fdda5510c88c4941a463a29ad7e394b91fdf11
Alexandre Queiroz <xandyzh65@gmail.com>
2023-11-10 21:31:34 -0300

    Baixar apenas o Audio
    Adicionado a opção de baixar apenas o áudio, além das cores para melhorar a interface e anotações para facilitar o entendimento.
    
    adicionado descrição do README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Youtube_Downloader
+**YouTube Downloader**
+
+Este é um script simples em Python para baixar vídeos ou áudios do YouTube. O usuário fornece o link do vídeo desejado, escolhe entre baixar o vídeo ou apenas o áudio e o download é iniciado. Utiliza a biblioteca Pytube para interagir com a API do YouTube. As mensagens de status durante o processo de download são coloridas para melhor visualização no terminal. Basta executar o script e seguir as instruções!
+
+**Instruções:**
+1. Execute o script.
+2. Insira o link do vídeo do YouTube.
+3. Escolha entre baixar o vídeo (V) ou apenas o áudio (A).
+4. Aguarde o download e aproveite!
+
+**Requisitos:**
+- Python 3.x
+- Pytube (instalável via `pip install pytube`)
+
+**Nota:** Este script é uma solução básica para baixar conteúdo do YouTube, e é fornecido sem garantias. Use-o conforme as políticas e termos de serviço do YouTube.
+
+**Aviso:** O download e uso de conteúdo protegido por direitos autorais sem permissão podem violar os termos de serviço do YouTube. Certifique-se de ter permissão para baixar e usar o conteúdo.
--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from pytube import YouTube
 
-while True:
-    link = input("Qual o link do vídeo que você quer baixar? ")
-    video = YouTube(link)
+# Solicita ao usuário um link
+link = input("Qual o link do vídeo que você quer baixar? ")
+video = YouTube(link)
 
-    # Obtém o título do vídeo
-    titulo = video.title
-    print(f'\nTítulo do vídeo: {titulo}\n')
+# Obtém o título do vídeo
+titulo = video.title
+print(f'\nTítulo do vídeo: \033[93m{titulo}\033[0m\n')
 
-    confirmacao = input(f'Esse é o titulo do video que você quer baixar? [S/N]\n')
-    if confirmacao == 's':
-        video = video.streams.get_highest_resolution()
-        video.download()
-        print(f'Pronto, vídeo baixado!')
+# Verifica a opção escolhida pelo usuário
+confirmacao = input(f'Quer baixar o vídeo ou só o áudio? [V/A]\n')
 
-    elif confirmacao == 'n':
-        continue
+# Se escolheu baixar o vídeo
+if confirmacao.lower() == 'v':
+    print("\033[94mBaixando o Vídeo...\033[0m\n")
+    ys = video.streams.get_highest_resolution()
 
-    else:
-        print('Opção inválida. Por favor, digite S ou N.')
+ # Se escolheu baixar apenas o áudio
+elif confirmacao.lower() == 'a':
+    print("\033[94mBaixando apenas o Áudio...\033[0m\n")
+    ys = video.streams.filter(only_audio=True).first()
+
+else:
+    print('\033[91mOpção inválida. Por favor, digite V ou A.\033[0m')
+    exit()
+
+# Inicia o download
+print("\033[92m***DOWNLOAD EM PROGRESSO***\033[0m\n")
+ys.download()
+print("\033[92m***DOWNLOAD CONCLUÍDO***\033[0m\n")
